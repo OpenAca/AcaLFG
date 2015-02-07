bac11bbff5a8cfce7dc13207eba557463625ef2a
Craig Martek <craigmartek@gmail.com>
2015-02-06 22:37:16 -0500

    starter bass template

--- acalfg/urls.py
+++ acalfg/urls.py
@@ -2,16 +2,12 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from django.views import generic
 
-
 admin.autodiscover()
 
-
 urlpatterns = patterns("",
-
-    # Admin URLs.
-    url(r"^admin/", include(admin.site.urls)),
-    
-    # There's no favicon here!
-    url(r"^favicon.ico$", generic.RedirectView.as_view()),
-    
+  # Admin URLs.
+  url(r"^admin/", include(admin.site.urls)),
+  # There's no favicon here!
+  url(r"^favicon.ico$", generic.RedirectView.as_view()),
+  url('^$', 'board.views.home'),
 )
